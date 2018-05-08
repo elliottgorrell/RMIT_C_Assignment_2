76537d4652f162f14bb077f92f40f1b360f6295e
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-05-08 12:26:30 +0930

    startup code

---
+++ vm.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+** Student name: 	...
+** Student number: 	...
+** Course: 			Advanced Programming Techniques - S1 2018
+******************************************************************************/
+
+#include "vm.h"
+
+int main(int argc, char ** argv)
+{
+    printf("Goodbye. \n\n");
+
+    return EXIT_SUCCESS;
+}
---
+++ vm_stock.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+** Student name: 	...
+** Student number: 	...
+** Course: 			Advanced Programming Techniques - S1 2018
+******************************************************************************/
+
+#include "vm_stock.h"
+
+/**
+ * vm_stock.c this is the file where you will implement the
+ * interface functions for managing the stock list.
+ **/
+
+/**
+ * Some example functions:
+ * create list, free list, create node, free node, insert node, etc...
+ */
